6e29212a55a73c6c251f0d75ae3e4afcd74d6043
Maged Rashed <engmagedr@gmail.com>
2022-09-19 21:55:07 +0200

    Task B2 completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:map_exam/note.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static Route route() => MaterialPageRoute(builder: (_) => const HomeScreen());
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final CollectionReference<Note> _collectionReference = FirebaseFirestore.instance
+      .collection("notes")
+      .doc(FirebaseAuth.instance.currentUser!.email)
+      .collection("Note documents")
+      .withConverter<Note>(
+        fromFirestore: (snapshot, _) => Note.fromJson(snapshot.data()!),
+        toFirestore: (note, _) => note.toJson(),
+      );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,47 +28,76 @@ class HomeScreen extends StatelessWidget {
         actions: [
           CircleAvatar(
             backgroundColor: Colors.blue.shade200,
-            child: const Text(
-              '4',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
-            ),
+            child: StreamBuilder<QuerySnapshot<Note>>(
+                stream: _collectionReference.snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return const SizedBox.shrink();
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Text("Loading");
+                  }
+                  return Text(
+                    '${snapshot.data?.docs.length}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                    ),
+                  );
+                }),
           ),
           const SizedBox(
             width: 10,
           ),
         ],
       ),
-      body: ListView.separated(
-        itemCount: 4,
-        separatorBuilder: (context, index) => const Divider(
-          color: Colors.blueGrey,
-        ),
-        itemBuilder: (context, index) => ListTile(
-          trailing: SizedBox(
-            width: 110.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.edit, color: Colors.blue),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(
-                    Icons.delete,
-                    color: Colors.blue,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-          title: const Text('Note title'),
-          subtitle: const Text('Note content'),
-          onTap: () {},
-          onLongPress: () {},
-        ),
-      ),
+      body: StreamBuilder<QuerySnapshot<Note>>(
+          stream: _collectionReference.snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.separated(
+                itemCount: snapshot.data!.docs.length,
+                separatorBuilder: (context, index) => const Divider(
+                      color: Colors.blueGrey,
+                    ),
+                itemBuilder: (context, index) {
+                  final note = snapshot.data!.docs[index].data();
+                  return ListTile(
+                    trailing: SizedBox(
+                      width: 110.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit, color: Colors.blue),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: const Icon(
+                              Icons.delete,
+                              color: Colors.blue,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                    title: Text(note.title!),
+                    subtitle: Text(note.content!),
+                    onTap: () {},
+                    onLongPress: () {},
+                  );
+                });
+          }),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
